472a06fd1a5b3a21cb03486fb781011bca1e8520
dzcrypton <35413673+dzcrypton@users.noreply.github.com>
2022-09-25 21:58:29 +0000

    Update s.sh

--- s.sh
+++ s.sh
@@ -4,9 +4,9 @@ apt-get -f install  -y \
         nano \
         psmisc \
         sudo \
-        tini cron \
-        software-properties-common python3 python3-dev python3-numpy \
-        openvpn autocutsel  python3-pip python3.9-tk python3.9-dev  pwgen  \
+         cron \
+        software-properties-common python3  python3-numpy \
+        openvpn autocutsel  python3-pip python3.9-dev  pwgen  \
         wget openssh-server locate nano gedit screen  net-tools curl git tor \
         xfce4-goodies xfce4\
         thunar tumbler xarchiver \
@@ -14,4 +14,4 @@ apt-get -f install  -y \
         dbus-x11 xauth xinit x11-xserver-utils xdg-utils \
         tightvncserver novnc python3-websockify python3-pyvirtualdisplay \
         build-essential \
-        xvfb xserver-xephyr ttf-wqy-zenhei python2.7
+        xvfb xserver-xephyr fonts-wqy-zenhei python2.7
